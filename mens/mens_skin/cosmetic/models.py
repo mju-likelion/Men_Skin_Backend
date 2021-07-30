edb82090a1d954e179b570962b08945715a21364
magmom95 <64140544+magmom95@users.noreply.github.com>
2021-07-30 09:13:06 +0900

    Add files via upload

--- mens/mens_skin/cosmetic/models.py
+++ mens/mens_skin/cosmetic/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+class Users(models.Model):
+    id_int = models.AutoField(primary_key=True)
+    users_email = models.CharField(max_length=30)
+    users_pwd = models.CharField(max_length=255)
+    users_nickname = models.CharField(max_length=30)
+    users_subscription = models.CharField(max_length=30, null=True)
+    users_skin_type = models.CharField(max_length=30, null=True)
+
+class Board(models.Model):
+    id_int=models.AutoField(primary_key=True)
+    board_writer = models.CharField(max_length=20)
+    board_title = models.CharField(max_length=20)
+    board_contents = models.TextField()
+    board_views = models.IntegerField(max_length=4)
+    board_create_at = models.DateTimeField(auto_now_add=True)
+    board_type = models.CharField(max_length=30)   
+    board_like = models.IntegerField(4) 
+
+class comment(models.Model):
+    comment_int = models.AutoField(primary_key=True)
+    id_int = models.AutoField()
+    user_nickname = models.CharField(30)
+    comment_content = models.CharField(100)
+    comment_writer = models.CharField(30)
+    board_create_at = models.DateTimeField(auto_now_add=True)
