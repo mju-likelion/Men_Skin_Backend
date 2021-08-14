b5ce254f9764545898f7abd84756550ff78d3d4f
magmom95 <magmom95@naver.com>
2021-08-15 02:07:47 +0900

    Delete middleware.py for settings on heroku

--- skin/settings.py
+++ skin/settings.py
@@ -64,7 +64,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'phople.middleware.DisableCSRF',
 ]
 
 ROOT_URLCONF = 'skin.urls'
